d650233801161a3ae5cee22b6a689451ad2c79a7
brandontrice <btrice9595@gmail.com>
2026-03-23 19:45:29 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 from database import db
 
 app = Flask(__name__)
 
-# Database config
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sunset_courts.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'sunsetcourts2025'
 
-# intialize db
 db.init_app(app)
 
-# create tables
 with app.app_context():
-	db.create_all()
-	print('Database tables created succcessfully.')
-
+    db.create_all()
+    print('Database tables created successfully.')
 
 @app.route('/')
 def index():
-	return 'Sunset Courts is running!'
+    return render_template('index.html')
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
