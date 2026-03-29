be2482a74d3350c9230a58ac2f4adf096141ef2d
bwm25040 <bwm25040@email.vccs.edu>
2026-03-28 20:31:10 -0400

    Update app.py

--- app.py
+++ app.py
@@ -429,14 +429,13 @@ def add_booking():
     start_time = datetime.strptime(data.get('start_time'), '%H:%M').time()
     end_time = datetime.strptime(data.get('end_time'), '%H:%M').time()
     has_guest = bool(data.get('has_guest', False))
-    #Restritics bookings to this year
     booking_date = date.fromisoformat(data.get('date'))
-        #current_year = date.today().year
-
-        #if booking_date.year != current_year:
-            #return jsonify({
-            #    'error': f'Bookings are only allowed for {current_year}.'
-            #}), 400
+    #Restritics bookings to this year
+    current_year = date.today().year
+        if booking_date.year != current_year:
+            return jsonify({
+                'error': f'Bookings are only allowed for {current_year}.'
+            }), 400
 
     start_total = start_time.hour * 60 + start_time.minute
     end_total = end_time.hour * 60 + end_time.minute
